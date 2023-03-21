eb7c18f25692bdd1cc5dca818b977a606e3bca4a
Miguel Angel <miguel.ampudia@outlook.es>
2023-03-20 20:48:23 -0500

    Update Program.cs

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,13 +11,13 @@ var app = builder.Build();
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 20032023-v1.0.0 CI/CD in AKS v1.0.0 Miguel Angel Ampudia Gomez";
+    return "GET - Net 6 Running ... 20032023-v1.0.0 CI/CD in AKS v1.0.0 Miguel Angel Ampudia Gomez PIPELINE";
 });
 
 app.MapPost("/", () =>
 {
     app.Logger.LogInformation("Initial Home Post");
-    return "POST - Net 6 Running ... 20032023-v1.0.0 CI/CD in AKS v1.0.0 Miguel Angel Ampudia Gomez";
+    return "POST - Net 6 Running ... 20032023-v1.0.0 CI/CD in AKS v1.0.0 Miguel Angel Ampudia Gomez PIPELINE";
 });
 
 //app.Run($"http://0.0.0.0:{port}");
